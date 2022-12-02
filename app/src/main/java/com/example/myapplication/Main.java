f9198cb76ab59ac8cee39fa7940e93513602c222
LeeJin0801 <leetrue801@gmail.com>
2022-12-02 16:40:47 +0900

    rank system 완

--- app/src/main/java/com/example/myapplication/Main.java
+++ app/src/main/java/com/example/myapplication/Main.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import com.example.myapplication.adapter.BestCurriculumAdapter;
+
+import java.util.ArrayList;
 
 public class Main extends AppCompatActivity{
 
@@ -23,6 +30,25 @@ public class Main extends AppCompatActivity{
         moveHobby.setOnClickListener(hobby);
         moveLife = (CardView) findViewById(R.id.life_button);
         moveLife.setOnClickListener(life);
+
+        ListView listView = findViewById(R.id.main_listview);
+
+        DBHelper helper = new DBHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select id,title,name from curriculum order by best desc", null);
+
+        ArrayList<BestCurriculumVO> data = new ArrayList<>();
+        while(cursor.moveToNext()){
+            BestCurriculumVO vo = new BestCurriculumVO();
+            vo.id = cursor.getString(0);
+            vo.title = cursor.getString(1);
+            vo.name = cursor.getString(2);
+            data.add(vo);//arraylist에 id, title, name 담은것 추가
+        }
+
+        db.close();
+        BestCurriculumAdapter adapter = new BestCurriculumAdapter(this, R.layout.best_item, data);
+        listView.setAdapter(adapter);
     }
     View.OnClickListener study = new View.OnClickListener() {
         @Override
