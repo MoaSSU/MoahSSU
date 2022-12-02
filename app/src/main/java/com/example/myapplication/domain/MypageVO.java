37a87caac77c2ec678ae981cae8e7b690ce6d9b3
GimHaLim <halim7568@gmail.com>
2022-12-02 19:08:29 +0900

    mypage

---
+++ app/src/main/java/com/example/myapplication/domain/MypageVO.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.domain;
+
+public class MypageVO {
+    public String title;
+    public String difficulty;
+    public String imageUrl;
+
+    public String getTitle() {return title; }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDifficulty() {return difficulty;}
+    public void setDifficulty(String difficulty) { this.difficulty =difficulty; }
+    public String getImageUrl() { return imageUrl; }
+    public void setImageUrl(String imageUrl) {this.imageUrl = imageUrl;}
+}
