d99c412c6a35826d8f4d44f309a9dbc8743106a2
GimHaLim <halim7568@gmail.com>
2022-12-07 19:17:05 +0900

    search

--- app/src/main/java/com/example/myapplication/SearchActivity.java
+++ app/src/main/java/com/example/myapplication/SearchActivity.java
@@ -1,14 +1,24 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.SearchView;
+
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.myapplication.R;
 
 public class SearchActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> launcher;
+    public SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        searchView = findViewById(R.id.search_bar);
+
     }
 }
